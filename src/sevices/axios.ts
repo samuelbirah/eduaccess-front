01d800b750cdc3b16c84fd29074785a19b3c7d4f
samuelbirah <samuel.biraheka@2027.ulc-icam.com>
2025-04-10 01:40:09 +0100

    temp

--- src/sevices/axios.ts
+++ src/sevices/axios.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:5000/api",
+    baseURL: "https://eduaccess-back.onrender.com/api",
+    // baseURL: "http://localhost:5000/api",
     withCredentials: true,
 });
 
